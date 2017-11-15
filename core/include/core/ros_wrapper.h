7adb755b539f95ac131b4ad9f9a3904e2db832cc
Ke Sun <sunke.polyu@gmail.com>
2017-11-15 11:31:10 -0500

    Use ros wrapper instead of generic driver

---
+++ core/include/core/ros_wrapper.h
@@ -0,0 +1,91 @@
+#ifndef ROS_WRAPPER_H
+#define ROS_WRAPPER_H
+
+#include <vector>
+
+#include <ros/ros.h>
+#include <image_transport/image_transport.h>
+#include <cv_bridge/cv_bridge.h>
+#include <sensor_msgs/Image.h>
+#include <nav_msgs/Odometry.h>
+#include <message_filters/subscriber.h>
+#include <message_filters/time_synchronizer.h>
+
+namespace MARS {
+
+struct Generic_Image {
+  unsigned char * data;
+  int height;
+  int width;
+  int stride;
+  double timestamp;
+};
+
+struct Generic_IMU {
+  double accelX;
+  double accelY;
+  double accelZ;
+
+  double gyroX;
+  double gyroY;
+  double gyroZ;
+
+  double timestamp;
+};
+
+class GenericDriver {
+public:
+  GenericDriver(const ros::NodeHandle& n);
+  GenericDriver(const GenericDriver&) = delete;
+  GenericDriver operator=(const GenericDriver&) = delete;
+
+  // Interface required by the mars_core library.
+  void startCamera(int cam_id);
+  void getImage(struct Generic_Image* img, int cam_id);
+
+  void startIMU();
+  void getIMU(Generic_IMU* imu);
+
+  //typedef boost::shared_ptr<GenericDriver> Ptr;
+  //typedef boost::shared_ptr<const GenericDriver> ConstPtr;
+
+private:
+
+  // Imu message callback.
+  void imuCallback(
+      const sensor_msgs::ImuConstPtr& msg);
+
+  // Stereo image callback.
+  void stereoCallback(
+      const sensor_msgs::ImageConstPtr& cam0_img,
+      const sensor_msgs::ImageConstPtr& cam1_img);
+
+
+  // Imu message buffer.
+  std::vector<sensor_msgs::Imu> img_msg_buffer;
+
+  // Latest stereo images.
+  cv_bridge::CvImageConstPtr cam0_img_ptr;
+  cv_bridge::CvImageConstPtr cam1_img_ptr;
+
+
+  // Ros node handle.
+  ros::NodeHandle nh;
+
+  // Subscribers
+  message_filters::Subscriber<
+    sensor_msgs::Image> cam0_img_sub;
+  message_filters::Subscriber<
+    sensor_msgs::Image> cam1_img_sub;
+  message_filters::TimeSynchronizer<
+    sensor_msgs::Image, sensor_msgs::Image> stereo_sub;
+  ros::Subscriber imu_sub;
+
+};
+
+//typedef GenericDriver::Ptr GenericDriverPtr;
+//typedef GenericDriver::ConstPtr GenericDriverConstPtr;
+
+} // end namespace MARS
+
+#endif
---
+++ example_program/ros_wrapper.cpp
@@ -0,0 +1,101 @@
+#include <core/ros_wrapper.h>
+
+namespace MARS {
+
+GenericDriver::GenericDriver(ros::NodeHandle& n): nh(n) {
+
+  // Create subscribers.
+  imu_sub.subscribe(nh, "imu", 200);
+
+  cam0_img_sub.subscribe(nh, "cam0_image", 10);
+  cam1_img_sub.subscribe(nh, "cam1_image", 10);
+
+  stereo_sub.connectInput(cam0_img_sub, cam1_img_sub);
+  stereo_sub.registerCallback(&GenericDriver::stereoCallback, this);
+
+  return;
+}
+
+void GenericDriver::startCamera(int cam_id) {
+  return;
+}
+
+void GenericDriver::getImage(
+    struct Generic_Image* img, int cam_id) {
+
+  img->height = 800;
+  img->width = 960;
+  img->stride = img->width;
+
+  free(img->data);
+  img->data = (unsigned char*) malloc(
+      width * height * sizeof(unsigned char));
+  cv::Mat cv_img(img->height, img->width, CV_8UC1, img->data);
+
+  if (cam_id == 0) {
+
+    img->timestamp = cam0_img->header.stamp.toSec();
+    cam0_img->image.copyTo(cv_img);
+
+  } else if (cam_id == 1) {
+
+    img->timestamp = cam1_img->header.stamp.toSec();
+    cam1_img->image.copyTo(cv_img);
+
+  } else {
+    ROS_ERROR("Cannot find camera %d...", cam_id);
+  }
+
+  return;
+}
+
+void GenericDriver::startIMU() {
+  return;
+}
+
+void GenericDriver::getIMU(Generic_IMU* imu) {
+
+  // Get the oldest IMU msg.
+  const sensor_msgs::Imu& imu_msg = imu_msg_buffer[0];
+
+  // Set the imu data.
+  imu->timestamp = imu_msg.header.stamp.toSec();
+
+  imu->gyroX = imu_msg.angular_velocity.x;
+  imu->gyroY = imu_msg.angular_velocity.y;
+  imu->gyroZ = imu_msg.angular_velocity.z;
+
+  imu->accelX = imu_msg.linear_acceleration.x;
+  imu->accelY = imu_msg.linear_acceleration.y;
+  imu->accelZ = imu_msg.linear_acceleration.z;
+
+  // Remove the used imu msg.
+  imu_msg_buffer.erase(imu_msg_buffer.begin());
+
+  return;
+}
+
+void GenericDriver::imuCallback(
+    const sensor_msgs::ImuConstPtr& msg) {
+
+  // Save the imu msgs into a buffer.
+  imu_msg_buffer.push_back(*msg);
+
+  return;
+}
+
+void GenericDriver::stereoCallback(
+    const sensor_msgs::ImageConstPtr& cam0_img,
+    const sensor_msgs::ImageConstPtr& cam1_img) {
+
+  // Save the latest stereo images.
+  cam0_img_ptr = cv_bridge::toCvShare(cam0_img,
+      sensor_msgs::image_encodings::MONO8);
+  cam1_img_ptr = cv_bridge::toCvShare(cam1_img,
+      sensor_msgs::image_encodings::MONO8);
+
+  return;
+}
+
+
+} // end namespace MARS
